20084d0fada6a6c3c5a5fe9264418c81f2f9f333
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-05-01 12:55:32 +0600

    go

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
   return (
     <>
-    <h1 className='bg-dark text-info border mt-4'>
+    <h1 className='bg-dark text-info border border-dark mt-4'>
       new item
     </h1>
     </>
