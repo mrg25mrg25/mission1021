3fa37c62c6f9e4ae5b80c927988dedd8fcb77a0a
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-05-01 13:02:23 +0600

    lets see

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const index = () => {
   return (
     <>
-  <div className="first border ">
+  <div className="first border d-flex justifi-content-center align-item-center ">
     <div className="left col-6">
-      <h1>yeah whatever</h1>
+      <h1 className='text-center'>yeah whatever</h1>
     </div>
     <div className="right col-6">
-      <h1>lets cook bitch</h1>
+      <h1 className='text-center'>lets cook bitch</h1>
     </div>
   </div>
     </>
