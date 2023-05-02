1b1712e4818d72fd4d1dca65aeb56c61677201be
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-05-03 02:09:36 +0600

    ol

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from './nicola/Navbar'
+
 import Top_docu from './nicola/Top_docu'
 
 const index = () => {
   return (
     <div>
-      <Navbar/>
+
       <Top_docu/>
     </div>
   )
