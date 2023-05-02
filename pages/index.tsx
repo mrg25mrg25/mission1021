ed468ad9a2eec0fe8e9c7f8a0c74c4b8cb2df312
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-05-02 20:26:43 +0600

    woho

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const index = () => {
     <>
   <div className="first border d-flex justifi-content-center align-item-center ">
     <div className="left col-6">
-      <h1 className='text-center text-danger'>yeah whatever</h1>
+      <h1 className='text-center text-info'>yeah whatever</h1>
     </div>
     <div className="right col-6">
       <h1 className='text-center text-info '>lets cook bitch</h1>
